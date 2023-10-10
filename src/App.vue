9361454ad600742810a3318e1b22f4031e510ac7
Jesvi Jonathan <jesvi22j@gmail.com>
2023-10-11 03:24:26 +0530

    mobile device alert

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,33 @@ import Footer from "./components/footer.vue";
 import HeaderBar from "./components/headingbar.vue";
 
 import sjson from "./assets/search.json";
+
+if (window.innerWidth < 800) {
+  document.body.innerHTML = `<div style='
+    height: 100%;
+        width: 100%;
+        font-size: 3vw;
+        font-family: Poppins, sans-serif;
+        font-weight: 600;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        align-items: center;
+        font-family: cocoon;
+        justify-content: center;
+        color: black;
+        z-index: 10;
+        position: fixed;
+        flex-direction: column;
+        text-align: center;
+        
+  '>
+    Sorry, this website is not available on mobile devices.
+    <br/><br/>
+    Use a desktop or laptop to view this website.
+  </div>`;
+}
 </script>
 
 <template>
